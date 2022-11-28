532c425667250742e41ac37676c33889d62f1eee
abc <abc@gmail.com>
2022-11-28 22:49:51 +0530

    First Commit

---
+++ FootballLeagueManagementSystem/FootballLeagueManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using FootballLeagueManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FootballLeagueManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        SportsEntities db = new SportsEntities();
+        private object m;
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = db.FootBallLeagues.ToList();
+            return View(data);
+        }
+        public ActionResult Display()
+        {
+            var d = from  i in  FootBallLeague  where SportsEntities.Status == "Win" select m;
+            return View(d);
+        }
+    }
+}
